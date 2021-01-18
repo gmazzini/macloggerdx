0eb0e652b329ed6fcef306274286fb909cdc0c34
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-18 11:16:36 +0100

    Update hadx.php

--- hadx.php
+++ hadx.php
@@ -10,14 +10,17 @@ $mycont=$mys["cont"];
 $db=new SQLite3("../MacLoggerDX.sql");
 $mm=array("USB"=>"PH","LSB"=>"PH","CW"=>"CW","FT8"=>"DG","MFSK"=>"DG");
 
-$res = $db->query("SELECT call,band_tx,dxcc_id,mode FROM qso_table_v007 where contest_id='$mycontest' limit 20");
+$res = $db->query("SELECT call,band_tx,mode FROM qso_table_v007 where contest_id='$mycontest'");
 while ($row = $res->fetchArray()) {
   
   $mys=findcall($row["call"]);
-  var_dump($row);
   
   $myid=$row["band_tx"]."-".$mm[$row["mode"]]."-".$row["call"];
   if(!isset($qso[$myid]))$qso[$myid]=1;
+  if($mys["base"]=="HA")$pp=10;
+  else if($mys["cont"]!=$mycont)$pp=5;
+  else $pp=2;
+  if(!isset($point[$myid]))$point[$myid]=$pp;
  
   $myid=$row["band_tx"]."-".$mys["base"];
   if(!isset($mult[$myid]))$mult[$myid]=1;
@@ -25,6 +28,7 @@ while ($row = $res->fetchArray()) {
 }
 
 echo array_sum($qso)." qso\n";
+echo array_sum($point)." point\n";
 echo array_sum($mult)." mult\n";
 
 ?>
