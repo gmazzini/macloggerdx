bc757ed6c09b669dc82a67fc1371908496b46703
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-18 11:11:54 +0100

    Update hadx.php

--- hadx.php
+++ hadx.php
@@ -5,20 +5,21 @@ $mycall="IK4LZH";
 
 include("country.php");
 $mys=findcall($mycall);
-print_r($zz[$mys]);
+$mycont=$mys["cont"];
 
 $db=new SQLite3("../MacLoggerDX.sql");
 $mm=array("USB"=>"PH","LSB"=>"PH","CW"=>"CW","FT8"=>"DG","MFSK"=>"DG");
 
-$res = $db->query("SELECT call,band_tx,dxcc_id,mode FROM qso_table_v007 where contest_id='$mycontest' limit 1");
+$res = $db->query("SELECT call,band_tx,dxcc_id,mode FROM qso_table_v007 where contest_id='$mycontest' limit 20");
 while ($row = $res->fetchArray()) {
   
+  $mys=findcall($row["call"]);
   var_dump($row);
   
   $myid=$row["band_tx"]."-".$mm[$row["mode"]]."-".$row["call"];
   if(!isset($qso[$myid]))$qso[$myid]=1;
  
-  $myid=$row["band_tx"]."-".$row["dxcc_id"];
+  $myid=$row["band_tx"]."-".$mys["base"];
   if(!isset($mult[$myid]))$mult[$myid]=1;
 
 }
