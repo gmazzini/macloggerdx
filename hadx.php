9cc10d867049c94a007bd9cb46e715b93682a419
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-18 16:05:04 +0100

    Update hadx.php

--- hadx.php
+++ hadx.php
@@ -1,12 +1,11 @@
 <?php
-// v0... just start to write by IK4LZH, usage: cd /$HOME/Documents/MLDX_Logs; php hadx.php
+// v1 by IK4LZH, usage: cd /$HOME/Documents/MLDX_Logs; php hadx.php
 $mycontest="hadx 2020";
 $mycall="IK4LZH";
 
 include("country.php");
 $mys=findcall($mycall);
 $mycont=$mys["cont"];
-
 $db=new SQLite3("../MacLoggerDX.sql");
 $mm=array("USB"=>"PH","LSB"=>"PH","CW"=>"CW","FT8"=>"DG","MFSK"=>"DG");
 
@@ -31,7 +30,6 @@ while ($row = $res->fetchArray()) {
     echo "$call $myid\n";
     if(!isset($mult[$myid]))$mult[$myid]=1;
   }
-
 }
 
 echo array_sum($qso)." qso\n";
