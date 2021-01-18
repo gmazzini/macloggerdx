adf603f026599612831a243c39574a5c36c67ba9
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-18 07:17:05 +0100

    Update hadx.php

--- hadx.php
+++ hadx.php
@@ -9,7 +9,7 @@ $mm=array("USB"=>"PH","LSB"=>"PH","CW"=>"CW","FT8"=>"DG","MFSK"=>"DG");
 $res = $db->query("SELECT call,band_tx,dxcc_id,mode FROM qso_table_v007 where contest_id='$mycontest' limit 4");
 while ($row = $res->fetchArray()) {
  
- $myid=$row["call"]."-".$row["band_tx"]."-".$mm[$row["mode"]];
+ $myid=$row["band_tx"]."-".$mm[$row["mode"]]."-".$row["call"];
  echo $myid."\n";
  if(!isset($qso[$myid]))$qso[$myid]=1;
  else $qso[$myid]++;
