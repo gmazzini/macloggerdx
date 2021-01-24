c4e20ac829b532412b017704deaaecd135d192a9
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-24 10:51:17 +0100

    Update today.php

--- today.php
+++ today.php
@@ -1,9 +1,9 @@
 <?php
 // v1 by IK4LZH, usage: cd /$HOME/Documents/MLDX_Logs; php macloggerdx/today.php
 
-$zz=strtotime("today midnigth");
+$zz=strtotime("today midnight");
 $db=new SQLite3("MacLoggerDX.sql");
 $res = $db->query("SELECT count(qso_start) FROM qso_table_v007 where qso_start>=$zz");
 $row = $res->fetchArray();
-echo "QSOs: ".$row0."\n";
+echo "QSOs: ".$row[0]."\n";
 ?>
