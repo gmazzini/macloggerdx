15a5fdab63119e229e18b8345714a116627801d3
Gianluca Mazzini <gianluca@mazzini.org>
2021-02-01 21:28:22 +0100

    Update tomysql.php

--- tomysql.php
+++ tomysql.php
@@ -5,8 +5,8 @@ $db=new SQLite3("MacLoggerDX.sql");
 
 $res = $db->query("SELECT call,band_tx,mode,qso_done,rst_sent,rst_received FROM qso_table_v007 order by qso_done limit 5");
 while ($row = $res->fetchArray()) {
-  $aux="insert into qso (callsign,band,mode,data,time,rst_sent,rst_rcvd) value \
-  ('".$row["callsign"]."',".$row["band"].",'".$row["mode"]."','".date("Ymd",$row["qso_done"])."'," \
+  $aux="insert into qso (callsign,band,mode,data,time,rst_sent,rst_rcvd) value 
+  ('".$row["callsign"]."',".$row["band"].",'".$row["mode"]."','".date("Ymd",$row["qso_done"])."',"
   '".date("His",$row["qso_done"])."','".$row["rst_sent"]."','".$row["rst_received"]."')";
   
   echo $aux;
