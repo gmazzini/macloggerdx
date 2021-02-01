f35965110bd0181190e0025e364053d12e132e9d
Gianluca Mazzini <gianluca@mazzini.org>
2021-02-01 19:00:51 +0100

    Update tomysql.php

--- tomysql.php
+++ tomysql.php
@@ -1,1 +1,16 @@
+<?php
+// v0 20210201 by IK4LZH
 
+$db=new SQLite3("MacLoggerDX.sql");
+
+$res = $db->query("SELECT call,band_tx,mode,qso_done,rst_sent,rst_received FROM qso_table_v007 order by qso_done limit 5");
+while ($row = $res->fetchArray()) {
+  
+ echo $mode." "; // mode 2c+s
+ $date=$row["qso_done"];
+ echo date("Y-m-d Hi",$date)." "; // date time 15c+s
+
+}
+
+$db->close();
+?>
