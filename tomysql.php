ddf3ebebf777d93ef195d6a3ee921b9ed49457f8
Gianluca Mazzini <gianluca@mazzini.org>
2021-02-01 22:24:35 +0100

    Update tomysql.php

--- tomysql.php
+++ tomysql.php
@@ -2,12 +2,12 @@
 // v0 20210201 by IK4LZH
 $xx="20201220";
 $flag=10;
-include("dati.php");
 
+include("dati.php");
 $conn=mysqli_connect($myhost,$myuser,$mypasswd,$mytable);
 $zz=mktime(0,0,0,substr($xx,4,2),substr($xx,6,2),substr($xx,0,4));
 $db=new SQLite3("MacLoggerDX.sql");
-$res = $db->query("SELECT call,band_tx,mode,qso_done,rst_sent,rst_received FROM qso_table_v007 where qso_done>$zz limit 30");
+$res = $db->query("SELECT call,band_tx,mode,qso_done,rst_sent,rst_received FROM qso_table_v007 where qso_done>$zz");
 while ($row = $res->fetchArray()) {
   $aux="insert into qso (callsign,band,mode,data,time,rst_sent,rst_rcvd,flag) value ".
   "('".$row["call"]."',".substr($row["band_tx"],0,-1).",'".$row["mode"]."','".date("Ymd",$row["qso_done"])."','".
