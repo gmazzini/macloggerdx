1083239f141b62b577a32198519a0052cf2b9eb8
Gianluca Mazzini <gianluca@mazzini.org>
2021-02-01 22:23:13 +0100

    Update tomysql.php

--- tomysql.php
+++ tomysql.php
@@ -1,6 +1,7 @@
 <?php
 // v0 20210201 by IK4LZH
 $xx="20201220";
+$flag=10;
 include("dati.php");
 
 $conn=mysqli_connect($myhost,$myuser,$mypasswd,$mytable);
@@ -8,9 +9,9 @@ $zz=mktime(0,0,0,substr($xx,4,2),substr($xx,6,2),substr($xx,0,4));
 $db=new SQLite3("MacLoggerDX.sql");
 $res = $db->query("SELECT call,band_tx,mode,qso_done,rst_sent,rst_received FROM qso_table_v007 where qso_done>$zz limit 30");
 while ($row = $res->fetchArray()) {
-  $aux="insert into qso (callsign,band,mode,data,time,rst_sent,rst_rcvd) value ".
+  $aux="insert into qso (callsign,band,mode,data,time,rst_sent,rst_rcvd,flag) value ".
   "('".$row["call"]."',".substr($row["band_tx"],0,-1).",'".$row["mode"]."','".date("Ymd",$row["qso_done"])."','".
-  date("His",$row["qso_done"])."','".$row["rst_sent"]."','".$row["rst_received"]."')";
+  date("His",$row["qso_done"])."','".$row["rst_sent"]."','".$row["rst_received"]."',".$flag.")";
   
   mysqli_query($conn,$aux);
   echo "$aux\n";
