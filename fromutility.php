e081cd63447e08bf8fcdcbbb9b830e80b8c8ae99
Gianluca Mazzini <gianluca@mazzini.org>
2021-02-02 16:59:38 +0100

    Update fromutility.php

--- fromutility.php
+++ fromutility.php
@@ -1,20 +1,22 @@
 <?php
 
+<?php
+
 include("dati.php");
 include("ik4lzh/utility.php");
 
 $conn=mysqli_connect($myhost,$myuser,$mypasswd,$mytable);
 
-$res=mysqli_query($conn,"select distinct callsign from qso where dxcc=0");
+$res=mysqli_query($conn,"select distinct callsign from qso where dxcc=0 or cqzone=0 or ituzone=0");
 while($row=mysqli_fetch_assoc($res)){
   $call=$row["callsign"];
-  $mys=findcall($call)
+  $mys=findcall($call);
+  if($mys["base"]=="K")continue;
   $dxcc=(int)$mys["dxcc"];
   $cqzone=(int)$mys["cqzone"];
   $ituzone=(int)$mys["ituzone"];
-
-//  mysqli_query($conn,"update qso set dxcc=$dxcc,cqzone=$cqzone,ituzone=$ituzone where callsign='$mycall'");
-  echo "update qso set dxcc=$dxcc,cqzone=$cqzone,ituzone=$ituzone where callsign='$mycall'\n";
+  mysqli_query($conn,"update qso set dxcc=$dxcc,cqzone=$cqzone,ituzone=$ituzone where callsign='$call'");
+  echo "update qso set dxcc=$dxcc,cqzone=$cqzone,ituzone=$ituzone where callsign='$call'\n";
 
 }
 
