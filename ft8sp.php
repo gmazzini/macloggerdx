567d6f122a776791ca08cb94bed571e4eb782836
Gianluca Mazzini <gianluca@mazzini.org>
2021-05-14 07:16:25 +0200

    Update ft8sp.php

--- ft8sp.php
+++ ft8sp.php
@@ -2,7 +2,7 @@
 // v1 by IK4LZH 20210514
 // usage: cd /$HOME/Documents/MLDX_Logs; php macloggerdx/ft8sp.php > /$HOME/Downloads/ft8sp.cbr
 $mycall="IK4LZH"; // youcall
-$myfrom="20200103"
+$myfrom="20210505"
 $fromdate=mktime(0,0,0,(int)substr($myfrom,4,2)),(int)substr($myfrom,6,2)),(int)substr($myfrom,0,4)));
 
 $db=new SQLite3("MacLoggerDX.sql");
