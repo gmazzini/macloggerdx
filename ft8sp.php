925f48d1a04bffcd41599c818ec65e31f93bbe37
Gianluca Mazzini <gianluca@mazzini.org>
2021-05-14 05:45:30 +0200

    Update ft8sp.php

--- ft8sp.php
+++ ft8sp.php
@@ -10,7 +10,8 @@ $mm=array("USB"=>"PH","LSB"=>"PH","CW"=>"CW","FT8"=>"DG","MFSK"=>"DG");
 $res = $db->query("SELECT call,tx_frequency,mode,qso_start,my_call,rst_sent,rst_received FROM qso_table_v007 where contest_id='$mycontest' order by qso_start");
 while ($row = $res->fetchArray()) {
  if($row["my_call"]!=$mycall)continue;
- echo "<call:".strlen($row["call"]).">".$row["call"];
+ $aux=$row["call"]; echo "<call:".strlen($aux).">".$aux;
+ $aux=date("Ymd",$row["qso_start"]); echo "<qso_date:".strlen($aux).">".$aux;
  echo "<eor>\n";
 
  
