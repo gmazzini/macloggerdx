f854285719310ff28ac17479a9602218c9cb5cfb
Gianluca Mazzini <gianluca@mazzini.org>
2021-05-14 07:36:15 +0200

    Update ft8sp.php

--- ft8sp.php
+++ ft8sp.php
@@ -1,6 +1,6 @@
 <?php
 // v1 by IK4LZH 20210514
-// usage: cd /$HOME/Documents/MLDX_Logs; php macloggerdx/ft8sp.php > /$HOME/Downloads/ft8sp.cbr
+// usage: cd /$HOME/Documents/MLDX_Logs; php macloggerdx/ft8sp.php > /$HOME/Downloads/ft8sp.adif
 $mycall="IK4LZH"; // youcall
 $myfrom="20210505";
 
@@ -12,6 +12,7 @@ $db=new SQLite3("MacLoggerDX.sql");
 $res = $db->query("SELECT call,tx_frequency,mode,qso_start,my_call,rst_sent,rst_received FROM qso_table_v007 where qso_start>=$fromdate order by qso_start");
 while ($row = $res->fetchArray()) {
  if($row["my_call"]!=$mycall)continue;
+ if($row["mode"]!="FT8" && $row["mode"]!="MFSK")continue;
  $aux=$row["call"]; echo "<CALL:".strlen($aux).">".$aux;
  $aux=date("Ymd",$row["qso_start"]); echo "<QSO_START:".strlen($aux).">".$aux;
  $aux=date("Hi",$row["qso_start"]); echo "<TIME_ON:".strlen($aux).">".$aux;
