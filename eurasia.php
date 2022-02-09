7903f209b3ba2548d2b7f5a324b06c1b92544c11
Gianluca Mazzini <gianluca@mazzini.org>
2022-02-09 18:20:47 +0100

    Update eurasia.php

--- eurasia.php
+++ eurasia.php
@@ -16,27 +16,21 @@ echo "ADDRESS: xxx\n";
 $db=new SQLite3("MacLoggerDX.sql");
 $mm=array("USB"=>"PH","LSB"=>"PH","CW"=>"CW","FT8"=>"DG","MFSK"=>"DG");
 
-$res = $db->query("SELECT call,tx_frequency,mode,qso_start,my_call FROM qso_table_v007 where qso_start>=$from and qso_start<=$to order by qso_start");
+$res = $db->query("SELECT call,tx_frequency,mode,qso_start,my_call,grid,rst_sent FROM qso_table_v007 where qso_start>=$from and qso_start<=$to order by qso_start");
 while ($row = $res->fetchArray()) {
  if($row["my_call"]!=$mycall)continue;
- echo "QSO: "; // label 4c+s
+ echo "QSO: ";
  $freq=(int)($row["tx_frequency"]*1000);
  if($freq>30000)$freq=(int)($freq/1000); 
- echo $freq." "; // frequency 5c+s
+ echo $freq." ";
  $mode=$mm[$row["mode"]];
- echo $mode." "; // mode 2c+s
+ echo $mode." ";
  $date=$row["qso_start"];
- echo date("Y-m-d Hi",$date)." "; // date time 15c+s
- printf("%-13s ",$row["my_call"]); // 13c+s
- printf("%3s ",$row["rst_sent"]); // rst_sent 3c+s
- $myaux=(int)$row["stx_numeric"];
- if($myaux>0)printf("%03d %2s ",$myaux,$row["stx"]); // stx_sent 6c+s
- else printf("%6s ",$row["stx"]);
- printf("%-13s ",$row["call"]); // 13c+s
- printf("%3s ",$row["rst_received"]); // rst_sent 3c+s
- $myaux=(int)$row["srx_numeric"];
- if($myaux>0)printf("%03d %2s ",$myaux,$row["srx"]); // stx_sent 6c+s
- else printf("%6s ",$row["srx"]);
+ echo date("Y-m-d Hi",$date)." ";
+ printf("%-13s ",$row["my_call"]);
+ printf("%3s ",$row["rst_sent"]);
+ printf("%-13s ",$row["call"]);
+ printf("%3s ",$row["rst_received"]);
  echo "\n";
 }
 
