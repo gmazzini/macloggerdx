926a0140342aecb68b3cb16ccf862312285ca86c
Gianluca Mazzini <gianluca@mazzini.org>
2022-03-07 09:18:34 +0100

    Update fromqrz.php

--- fromqrz.php
+++ fromqrz.php
@@ -10,17 +10,18 @@ $conn=mysqli_connect($myhost,$myuser,$mypasswd,$mytable);
 
 $res=mysqli_query($conn,"select distinct callsign from qso where flag=$flag");
 while($row=mysqli_fetch_assoc($res)){
-  $mycall=$row["callsign"];
+  $mycall=strtok($row["callsign"]," ");
   $f2=file_get_contents("http://xmldata.qrz.com/xml/current/?s=$key;callsign=$mycall");
   $ret=simplexml_load_string($f2);
-  $dxcc=$ret->Callsign->dxcc;
-  $grid=$ret->Callsign->grid;
-  $email=$ret->Callsign->email;
-  $cqzone=$ret->Callsign->cqzone;
-  $ituzone=$ret->Callsign->ituzone;
-  mysqli_query($conn,"update qso set dxcc=$dxcc,grid='$grid',email='$email',cqzone=$cqzone,ituzone=$ituzone where callsign='$mycall'");
-  echo "update qso set dxcc=$dxcc,grid='$grid',email='$email',cqzone=$cqzone,ituzone=$ituzone where callsign='$mycall'\n";
-
+  if(!isset($ret->Session->Error)){
+    $dxcc=$ret->Callsign->dxcc;
+    $grid=$ret->Callsign->grid;
+    $email=$ret->Callsign->email;
+    $cqzone=$ret->Callsign->cqzone;
+    $ituzone=$ret->Callsign->ituzone;
+    mysqli_query($conn,"update qso set dxcc=$dxcc,grid='$grid',email='$email',cqzone=$cqzone,ituzone=$ituzone where callsign='$mycall'");
+    echo "update qso set dxcc=$dxcc,grid='$grid',email='$email',cqzone=$cqzone,ituzone=$ituzone where callsign='$mycall'\n";
+  }
 }
 
 mysqli_close($conn);
