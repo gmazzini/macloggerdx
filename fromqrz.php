0fe3d3b7cb713dcd7295ceab304198ff6b0ac4b7
Gianluca Mazzini <gianluca@mazzini.org>
2022-03-07 10:14:04 +0100

    Update fromqrz.php

--- fromqrz.php
+++ fromqrz.php
@@ -15,13 +15,13 @@ while($row=mysqli_fetch_assoc($res)){
   $f2=file_get_contents("http://xmldata.qrz.com/xml/current/?s=$key;callsign=$mycall");
   $ret=simplexml_load_string($f2);
   if(!isset($ret->Session->Error)){
-    $dxcc=$ret->Callsign->dxcc;
+    $dxcc=$ret->Callsign->dxcc; if($dxcc=="")$dxcc=0;
     $grid=$ret->Callsign->grid;
     $email=$ret->Callsign->email;
-    $cqzone=$ret->Callsign->cqzone;
-    $ituzone=$ret->Callsign->ituzone;
-    mysqli_query($conn,"update qso set dxcc=$dxcc,grid='$grid',email='$email',cqzone=$cqzone,ituzone=$ituzone,flag=$flag where callsign='$mycall'");
+    $cqzone=$ret->Callsign->cqzone; if($cqzone=="")$cqzone=0;
+    $ituzone=$ret->Callsign->ituzone; if($ituzone=="")$ituzone=0;
     echo "update qso set dxcc=$dxcc,grid='$grid',email='$email',cqzone=$cqzone,ituzone=$ituzone,flag=$flag where callsign='$mycall'\n";
+    mysqli_query($conn,"update qso set dxcc=$dxcc,grid='$grid',email='$email',cqzone=$cqzone,ituzone=$ituzone,flag=$flag where callsign='$mycall'");
   }
 }
 
