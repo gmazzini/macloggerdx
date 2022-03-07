8423251df2ce9d6ca093c201f83cc66db10ebf94
Gianluca Mazzini <gianluca@mazzini.org>
2022-03-07 23:08:22 +0100

    Update qrzemail.php

--- qrzemail.php
+++ qrzemail.php
@@ -1,15 +1,20 @@
 <?php
+// php macloggerdx/qrzmail.php > q4
 
 include("dati.php");
 $conn=mysqli_connect($myhost,$myuser,$mypasswd,$mytable);
 
-$res=mysqli_query($conn,"select distinct callsign from qso where qrzflag=1 limit 100");
+$res=mysqli_query($conn,"select callsign,email from qso where qrzmail=0 and length(email)>5 group by callsign limit 1000");
 while($row=mysqli_fetch_assoc($res)){
   $ret=explode(" ",$row["callsign"]);
   $call=$ret[0];
-  echo $call."\n";
+  $email=$row["email"];
+  echo $call.",".$email."\n";
+  mysqli_query($conn,"update qso set qrzmail=2 where callsign='$call'");
+
 }
 
 mysqli_close($conn);
 
 ?>
+
