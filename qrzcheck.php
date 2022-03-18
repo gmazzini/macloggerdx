114f3f9bd8735ade16699af6899fef660bdbbe5f
Gianluca Mazzini <gianluca@mazzini.org>
2022-03-18 08:05:42 +0100

    Update qrzcheck.php

--- qrzcheck.php
+++ qrzcheck.php
@@ -9,7 +9,8 @@ while($row=mysqli_fetch_assoc($res)){
   $call=$ret[0];
   $res1=mysqli_query($conn,"select count(*) from qso where qrzmail>0 and callsign='$call'");
   $row1=mysqli_fetch_assoc($res1);
-  print_r($row1);
+  $num=$row1["count(*)"];
+  if($num>0)echo $call."\n";
 }
 
 mysqli_close($conn);
